a911d1a3c793782a6d8be53497c1af7c03fae54c
Robert Romore <r.romore@gmail.com>
2024-05-05 10:51:25 -0500

    move mergeObjects to utils

--- src/lib/components/tanstack-svelte-table/table.svelte.ts
+++ src/lib/components/tanstack-svelte-table/table.svelte.ts
@@ -1,3 +1,4 @@
+import { mergeObjects } from '$lib/utils';
 import {
   createTable,
   type RowData,
@@ -43,7 +44,7 @@ export function createSvelteTable<TData extends RowData>(options: TableOptions<T
         options: Partial<TableOptions<TData>>
       ) => {
         return mergeObjects(defaultOptions, options);
-      }
+      },
     },
     options
   );
@@ -73,44 +74,4 @@ export function createSvelteTable<TData extends RowData>(options: TableOptions<T
   });
 
   return table;
-}
-
-/**
- * Merges objects together while keeping their getters alive.
- * Taken from SolidJS: {@link https://github.com/solidjs/solid/blob/24abc825c0996fd2bc8c1de1491efe9a7e743aff/packages/solid/src/server/rendering.ts#L82-L115}
- * */
-export function mergeObjects<T>(source: T): T;
-export function mergeObjects<T, U>(source: T, source1: U): T & U;
-export function mergeObjects<T, U, V>(source: T, source1: U, source2: V): T & U & V;
-export function mergeObjects<T, U, V, W>(
-  source: T,
-  source1: U,
-  source2: V,
-  source3: W
-): T & U & V & W;
-export function mergeObjects(...sources: any): any {
-  const target = {};
-  for (let i = 0; i < sources.length; i++) {
-    let source = sources[i];
-    if (typeof source === 'function') source = source();
-    if (source) {
-      const descriptors = Object.getOwnPropertyDescriptors(source);
-      for (const key in descriptors) {
-        if (key in target) continue;
-        Object.defineProperty(target, key, {
-          enumerable: true,
-          get() {
-            for (let i = sources.length - 1; i >= 0; i--) {
-              let v,
-                s = sources[i];
-              if (typeof s === 'function') s = s();
-              v = (s || {})[key];
-              if (v !== undefined) return v;
-            }
-          }
-        });
-      }
-    }
-  }
-  return target;
 }
