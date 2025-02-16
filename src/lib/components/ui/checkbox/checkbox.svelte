3d500b990bed7d3a86200da2e52b51aa5673e2e3
Robert Romore <r.romore@gmail.com>
2025-02-16 09:47:02 -0600

    update checkbox component

--- src/lib/components/ui/checkbox/checkbox.svelte
+++ src/lib/components/ui/checkbox/checkbox.svelte
@@ -1,33 +1,35 @@
 <script lang="ts">
 	import { Checkbox as CheckboxPrimitive, type WithoutChildrenOrChild } from "bits-ui";
-	import Check from "svelte-radix/Check.svelte";
-	import Minus from "svelte-radix/Minus.svelte";
+	import Check from "lucide-svelte/icons/check";
+	import Minus from "lucide-svelte/icons/minus";
 	import { cn } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
-		class: className,
 		checked = $bindable(false),
+		indeterminate = $bindable(false),
+		class: className,
 		...restProps
 	}: WithoutChildrenOrChild<CheckboxPrimitive.RootProps> = $props();
 </script>
 
 <CheckboxPrimitive.Root
+	bind:ref
 	class={cn(
-		"border-primary focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer box-content size-4 shrink-0 rounded-sm border shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
+		"border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer box-content size-4 shrink-0 rounded-sm border focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
 		className
 	)}
 	bind:checked
-	bind:ref
+	bind:indeterminate
 	{...restProps}
 >
-	{#snippet children({ checked })}
-		<span class="flex size-4 items-center justify-center text-current">
-			{#if checked === "indeterminate"}
+	{#snippet children({ checked, indeterminate })}
+		<div class="flex size-4 items-center justify-center text-current">
+			{#if indeterminate}
 				<Minus class="size-3.5" />
 			{:else}
 				<Check class={cn("size-3.5", !checked && "text-transparent")} />
 			{/if}
-		</span>
+		</div>
 	{/snippet}
 </CheckboxPrimitive.Root>
