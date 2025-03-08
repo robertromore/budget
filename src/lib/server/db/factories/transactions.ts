bf594d75c4345c9925483b2781c51d25c6e49670
Robert Romore <r.romore@gmail.com>
2025-03-08 14:23:19 -0600

    reuse some entities when testing; increase transaction max

--- src/lib/server/db/factories/transactions.ts
+++ src/lib/server/db/factories/transactions.ts
@@ -7,12 +7,29 @@ import { categoryFactory } from "./categories";
 
 export const transactionFactory = async (
   account?: { id: number },
-  count: number = faker.number.int({ min: 1, max: 10 })
+  count: number = faker.number.int({ min: 1, max: 50 })
 ): Promise<NewTransaction[]> => {
   const transactions_collection: NewTransaction[] = [];
   for (let i = 0; i < count; i++) {
-    const new_payee: Payee = await payeeFactory(1).then((payees) => payees[0]);
-    const new_category: Category = await categoryFactory(1).then((categories) => categories[0]);
+    const payees: Payee[] = [];
+    const categories: Category[] = [];
+
+    let new_payee: Payee;
+    let new_category: Category;
+    if (
+      payees.length === 0 ||
+      categories.length === 0 ||
+      faker.helpers.rangeToNumber({ min: 1, max: 100 }) % 2 === 0
+    ) {
+      new_payee = await payeeFactory(1).then((payees) => payees[0]);
+      payees.push(new_payee);
+      new_category = await categoryFactory(1).then((categories) => categories[0]);
+      categories.push(new_category);
+    } else {
+      new_payee = faker.helpers.arrayElement(payees);
+      new_category = faker.helpers.arrayElement(categories);
+    }
+
     const new_transaction = await db
       .insert(transactions)
       .values({
