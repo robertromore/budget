420708c743751680c6f963eb945d741bf68e3945
Robert Romore <r.romore@gmail.com>
2025-03-08 14:25:03 -0600

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,25 +6,22 @@
   import type { Snippet } from "svelte";
   import { categoriesContext, CategoriesState } from "$lib/states/categories.svelte";
   import { payeesContext, PayeesState } from "$lib/states/payees.svelte";
-  // import { dev } from '$app/environment';
-  // import { RenderScan } from 'svelte-render-scan';
-  import { newAccountDialog } from "$lib/states/global.svelte";
-  import { UseBoolean } from "$lib/hooks/use-boolean.svelte";
+  import { dev } from '$app/environment';
+  import { RenderScan } from 'svelte-render-scan';
   import AddAccountDialog from "$lib/components/dialogs/add-account-dialog.svelte";
 import { accountsContext, AccountsState } from "$lib/states/accounts.svelte";
   import DeleteAccountDialog from "$lib/components/dialogs/delete-account-dialog.svelte";
 
   let { data, children }: { data: LayoutData; children: Snippet } = $props();
-  const { payees, categories } = $derived(data);
+  const { accounts, payees, categories } = $derived(data);
+  accountsContext.set(new AccountsState((() => accounts)()));
   categoriesContext.set(new CategoriesState((() => categories)()));
   payeesContext.set(new PayeesState((() => payees)()));
-
-  newAccountDialog.set(new UseBoolean(false));
 </script>
 
-<!-- {#if dev}
+{#if dev}
 	<RenderScan />
-{/if} -->
+{/if}
 
 <AddAccountDialog/>
 <DeleteAccountDialog/>
