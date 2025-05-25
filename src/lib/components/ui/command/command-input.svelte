096e5ef9bf41bfbc326832f1da60df5297bbe800
Robert Romore <r.romore@gmail.com>
2025-05-25 09:03:17 -0500

    update command component

--- src/lib/components/ui/command/command-input.svelte
+++ src/lib/components/ui/command/command-input.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
   import { Command as CommandPrimitive } from "bits-ui";
-  import Search from "lucide-svelte/icons/search";
+  import Search from "@lucide/svelte/icons/search";
   import { cn } from "$lib/utils.js";
 
   let {
     ref = $bindable(null),
     class: className,
-    wrapperClass,
     value = $bindable(""),
     ...restProps
-  }: CommandPrimitive.InputProps & { wrapperClass?: string } = $props();
+  }: CommandPrimitive.InputProps = $props();
 </script>
 
-<div class={cn("flex items-center border-b px-3", wrapperClass)} data-command-input-wrapper="">
+<div class="flex items-center border-b px-3" data-command-input-wrapper="">
   <Search class="mr-2 size-4 shrink-0 opacity-50" />
   <CommandPrimitive.Input
     class={cn(
-      "flex h-10 w-full rounded-md bg-transparent py-3 text-base outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+      "placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-base outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
       className
     )}
     bind:ref
