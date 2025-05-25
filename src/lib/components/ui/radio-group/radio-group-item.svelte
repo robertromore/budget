d310dd3d00af72bc2d0ca1c50fb198a59b7cebdf
Robert Romore <r.romore@gmail.com>
2025-05-25 09:12:30 -0500

    update radio-group component

--- src/lib/components/ui/radio-group/radio-group-item.svelte
+++ src/lib/components/ui/radio-group/radio-group-item.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { RadioGroup as RadioGroupPrimitive, type WithoutChildrenOrChild } from "bits-ui";
-  import Circle from "lucide-svelte/icons/circle";
+  import Circle from "@lucide/svelte/icons/circle";
   import { cn } from "$lib/utils.js";
 
   let {
@@ -15,7 +15,7 @@
 <RadioGroupPrimitive.Item
   bind:ref
   class={cn(
-    "aspect-square size-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+    "border-primary text-primary focus-visible:ring-ring aspect-square size-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
     className
   )}
   {...restProps}
@@ -23,7 +23,7 @@
   {#snippet children({ checked })}
     <div class="flex items-center justify-center">
       {#if checked}
-        <Circle class="size-3.5 fill-primary" />
+        <Circle class="fill-primary size-3.5" />
       {/if}
     </div>
   {/snippet}
