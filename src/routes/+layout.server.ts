4a095df22bfa5d9e53eced3e37ecbffb08fb2a33
Robert Romore <r.romore@gmail.com>
2025-08-27 07:52:26 -0500

    Refactor load function for improved readability and maintainability

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -7,41 +7,46 @@ import { formInsertAccountSchema, formInsertScheduleSchema } from "$lib/schema";
 import { getLocalTimeZone, today } from "@internationalized/date";
 
 const thisday = today(getLocalTimeZone());
-export const load: LayoutServerLoad = async () => ({
-  accounts: await createCaller(await createContext()).accountRoutes.all(),
-  payees: await createCaller(await createContext()).payeeRoutes.all(),
-  categories: await createCaller(await createContext()).categoriesRoutes.all(),
-  schedules: await createCaller(await createContext()).scheduleRoutes.all(),
-  manageAccountForm: await superValidate(zod4(formInsertAccountSchema)),
-  manageScheduleForm: await superValidate(zod4(formInsertScheduleSchema)),
-  dates: [
-    {
-      value: thisday.subtract({ days: 1 }).toString(),
-      label: "1 day ago",
-    },
-    {
-      value: thisday.subtract({ days: 3 }).toString(),
-      label: "3 days ago",
-    },
-    {
-      value: thisday.subtract({ weeks: 1 }).toString(),
-      label: "1 week ago",
-    },
-    {
-      value: thisday.subtract({ months: 1 }).toString(),
-      label: "1 month ago",
-    },
-    {
-      value: thisday.subtract({ months: 3 }).toString(),
-      label: "3 months ago",
-    },
-    {
-      value: thisday.subtract({ months: 6 }).toString(),
-      label: "6 months ago",
-    },
-    {
-      value: thisday.subtract({ years: 1 }).toString(),
-      label: "1 year ago",
-    },
-  ],
-});
+export const load: LayoutServerLoad = async () => {
+  const ctx = await createContext();
+  const caller = createCaller(ctx);
+
+  return {
+    accounts: await caller.accountRoutes.all(),
+    payees: await caller.payeeRoutes.all(),
+    categories: await caller.categoriesRoutes.all(),
+    schedules: await caller.scheduleRoutes.all(),
+    manageAccountForm: await superValidate(zod4(formInsertAccountSchema)),
+    manageScheduleForm: await superValidate(zod4(formInsertScheduleSchema)),
+    dates: [
+      {
+        value: thisday.subtract({ days: 1 }).toString(),
+        label: "1 day ago",
+      },
+      {
+        value: thisday.subtract({ days: 3 }).toString(),
+        label: "3 days ago",
+      },
+      {
+        value: thisday.subtract({ weeks: 1 }).toString(),
+        label: "1 week ago",
+      },
+      {
+        value: thisday.subtract({ months: 1 }).toString(),
+        label: "1 month ago",
+      },
+      {
+        value: thisday.subtract({ months: 3 }).toString(),
+        label: "3 months ago",
+      },
+      {
+        value: thisday.subtract({ months: 6 }).toString(),
+        label: "6 months ago",
+      },
+      {
+        value: thisday.subtract({ years: 1 }).toString(),
+        label: "1 year ago",
+      },
+    ],
+  };
+};
